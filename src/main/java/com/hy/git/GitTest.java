b9399e2c44cba1ce0db336d586398e4d37d3feef
linmujian <2900817905@qq.com>
2022-09-03 22:03:06 +0800

    hot-fix commit1

--- src/main/java/com/hy/git/GitTest.java
+++ src/main/java/com/hy/git/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("GitTest!");
         System.out.println("GitTest!");
         System.out.println("GitTest!");
+        System.out.println("GitTest!");
     }
 
 }
